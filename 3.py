255d7297aac2359b04f55d463525b51912f2a696
Luka <42498785+madladafterhours@users.noreply.github.com>
2023-02-22 14:29:43 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+#Email validator
+#Make a program to check whether an email address is valid or not.
+#For instance, you could make sure that there are no spaces, that there is an @ symbol and a dot somewhere after it. Also check the length of the parts at the start, and that the end parts
+#of the address are not blank.
+#Extensions:
+#1. When an email address is found to be invalid, tell the user exactly what they did wrong with their email address rather than just saying it is invalid
+#2. Allow the user to choose to give a text file with a list of email addresses and have it process them all automatically
+
+#This program is so painfully trivial and tedious that I'm just not going to do it.
+#It does not demonstrate any particular skills other than checking for whether or not a string contains something.
